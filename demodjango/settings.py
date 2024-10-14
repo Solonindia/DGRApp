a2876cc6e6e6146e3a1f4e7d7546d4a22058ae83
Onmget <onm.get@solonindia.com>
2024-10-14 16:12:03 +0530

    removed staticfiles

--- demodjango/settings.py
+++ demodjango/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Uncomment this line
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
