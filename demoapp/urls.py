c0f52949fff8a368d8e981c689285622e413c66e
Onmget <onm.get@solonindia.com>
2024-10-18 12:33:56 +0530

    added gatepass

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -1,10 +1,10 @@
 # complaint/urls.py
 from django.urls import path
 from . import views
-from gatepass.views import visitor_log_view,visitor_log_list,download_visitor_log_pdf,delete_visitor_log
+from gatepass.views import visitor_log_view,visitor_log_list,download_visitor_log_pdf,delete_visitor_log,visitor_log_user_view,visitor_log_list_user
 
 urlpatterns = [
-    path('', views.redirect_to_home),  # Redirect root URL to /home/
+    path('', views.redirect_to_home),  
     path('home/', views.home_page, name='home'),
     path('superuser/signup/', views.signup_view, name='signup'),
     path('user/login/', views.login_view, name='login'),  
@@ -22,10 +22,12 @@ urlpatterns = [
     path('delete_complaint/<int:complaint_id>/', views.delete_complaint, name='delete_complaint'),
     path('complaint_analysis/', views.complaint_analysis, name='complaint_analysis'),
     path('complaints/<str:type>/<str:site_name>/', views.ComplaintDetailView, name='complaints_detail'),
-    path('register/', visitor_log_view, name='visitor_log'),  # Home page with the visitor log form
-    path('visitor-log/', visitor_log_list, name='visitor_log_list'),  # List of visitor logs
+    path('superuser/', visitor_log_view, name='visitor_log'),  # Home page with the visitor log form
+    path('superuser/visitor-log/', visitor_log_list, name='visitor_log_list'),  # List of visitor logs
+    path('user/visitor-log-list/', visitor_log_list_user, name='visitor_log_list_user'), 
     path('download/<int:log_id>/', download_visitor_log_pdf, name='download_visitor_log_pdf'),  # Download PDF
-    path('visitor-log/<int:log_id>/delete/', delete_visitor_log, name='delete_visitor_log'),  # New URL for delete
+    path('superuser/visitor-log/<int:log_id>/delete/', delete_visitor_log, name='delete_visitor_log'),  # New URL for delete
+    path('user/visitor-log/', visitor_log_user_view, name='visitor_log_user_view'),  # List of visitor logs
 
 ]
 
