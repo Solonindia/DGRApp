7025421c4bb1dcb0af376cf5bafec464ad83c4de
Onmget <onm.get@solonindia.com>
2024-10-28 10:05:06 +0530

    removed staticfiles Storage

--- demodjango/urls.py
+++ demodjango/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('gatepass/',include('gatepass.urls'))
 ]
 
-# Serve static files only if DEBUG is True
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
